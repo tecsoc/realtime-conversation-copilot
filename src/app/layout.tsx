9d547eefc79b8d2fafb6184064313337d3bf1411
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-09-04 08:22:12 +0900

    とりあえずリアルタイム録音っぽいことができるところまで実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.scss'
+import './globals.sass'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,46 @@
-"use client";
-import useWhisper from '@chengsokdara/use-whisper'
+"use client"
+
+import React, { useCallback } from "react";
+import { ReactMic } from 'react-mic';
+import styles from "./page.module.sass";
+
+const RecordStateObject = {
+  notStarted: 0,
+  recording: 1,
+  paused: 2,
+} as const;
+type RecordStateType = typeof RecordStateObject[keyof typeof RecordStateObject];
 
 export default function Home() {
-  const {
-    recording,
-    speaking,
-    transcribing,
-    transcript,
-    pauseRecording,
-    startRecording,
-    stopRecording,
-  } = useWhisper({
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_TOKEN,
-  })
+  const [isRecording, setIsRecording] = React.useState<RecordStateType>(RecordStateObject.notStarted);
+  const buttonOnClick = useCallback(() => {
+    setIsRecording((prev) => {
+      if (prev === RecordStateObject.notStarted) return RecordStateObject.recording;
+      if (prev === RecordStateObject.recording) return RecordStateObject.paused;
+      if (prev === RecordStateObject.paused) return RecordStateObject.recording;
+      return prev;
+    })
+  }, []);
+
+  const buttonText = React.useMemo(() => {
+    if (isRecording === RecordStateObject.notStarted) return "録音開始";
+    if (isRecording === RecordStateObject.recording) return "一時停止";
+    if (isRecording === RecordStateObject.paused) return "再開";
+    return "";
+  }, [isRecording]);
   
   return (
-    <div>
-      <p>Recording: {recording}</p>
-      <p>Speaking: {speaking}</p>
-      <p>Transcribing: {transcribing}</p>
-      <p>Transcribed Text: {transcript.text}</p>
-      <button onClick={() => startRecording()}>Start</button>
-      <button onClick={() => pauseRecording()}>Pause</button>
-      <button onClick={() => stopRecording()}>Stop</button>
+    <div className={styles.parentNode}>
+      <ReactMic
+        record={isRecording === RecordStateObject.recording}
+        pause={isRecording === RecordStateObject.paused}
+        visualSetting="sinewave"
+        mimeType="audio/webm"
+        echoCancellation={true} 
+        autoGainControl={true}  
+        noiseSuppression={true} 
+      />
+      <button onClick={buttonOnClick}>{buttonText}</button>
     </div>
   )
 }
